21b4ba8a04bbc48ba4e46aa795b544aae045641e
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-30 19:54:40 +0530

    commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const userModel = require("./models/user");
+const postModel= require("./models/post") ;
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -14,14 +16,27 @@ app.use(cookieParser());
 app.get("/", function (req, res) {
     res.render("index")
 });
+
 app.post("/register", async function (req, res) {
     let { email, password, username, age, name } = req.body;
     let user = await userModel.findOne({ email })
     if (user) return res.status(500).send("User already registered")
 
     bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(password)
-    })
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let user = await userModel.create({
+                username,
+                email,
+                name,
+                age,
+                password: hash
+            });
+            let token = jwt.sign({ email: email, userid: user._id }, "hello")
+            res.cookie("token", token)
+            res.send("registered")
+
+        });
+    });
 
 
 });
--- models/user.js
+++ models/user.js
@@ -7,7 +7,10 @@ const userSchema = mongoose.Schema({
     name: String,
     email: String,
     password: String,
-    age: Number
+    age: Number,
+    post: [{
+        type: mongoose.Schema.Types.ObjectId, ref:"post"
+    }]
 })
 
 module.exports = mongoose.model('user', userSchema);
